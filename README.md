f470dcea65268dc2986110d4879d0aec410ed972
JohnNgethe <Jngethe07@gmail.com>
2023-09-19 15:56:18 +0300

    Final Project Done

---
+++ README.md
@@ -0,0 +1,23 @@
+This is a simple peronal web journal
+To run it:
+first install the packages recorded on the package.json file by running npm i or npm install
+Its built using ejs and node.
+Other bulding blocks include bootsrap version 3.3.7
+This project is designed to help furthe understand ejs and tmplating. 
+
+
+Resources
+1.Templating
+EJS <https://ejs.co/>
+
+2. Routing and server language
+Nodejs <https://nodejs.org/en/docs>
+Express Routing
+<https://expressjs.com/en/guide/routing.html>
+<https://expressjs.com/en/guide/routing.html#route-parameters>
+
+3.Content Generator
+Loren Ipsum Generator <https://loremipsum.io/>
+
+4. Utilities
+lodash <https://lodash.com/> :this will turn everything to lower case while ignoring dashes(-), underscores(_)
---
+++ app.js
@@ -0,0 +1,97 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+
+const aboutContent =
+  "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent =
+  "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+
+mongoose.connect(process.env.DB_URI, {
+  useNewUrlParser: true,
+});
+
+const postSchema = {
+  title: {
+    type: String
+  },
+  content: {
+    type: String
+  }
+};
+
+
+const Post = mongoose.model("Post", postSchema);
+
+app.get("/", async (req, res) => {
+  try {
+    const posts = await Post.find({});
+    res.render("home", {
+            posts: posts,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("An error occurred while fetching posts.");
+  }
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { aboutCont: aboutContent });
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact", { contactCont: contactContent });
+});
+
+app.get("/compose", (req, res) => {
+  res.render("compose");
+});
+
+app.post("/compose", (req, res) => {
+  const post = new Post({
+    title: req.body.postTitle,
+    content: req.body.postBody
+  });
+  post.save();
+  res.redirect("/");
+});
+
+app.get("/posts/:postId", async (req, res) => {
+  const requestedPostId = req.params.postId;
+
+  try {
+    const post = await Post.findOne({ _id: requestedPostId });
+
+    if (post) {
+      res.render("post", {
+        title: post.title,
+        content: post.content,
+      });
+    } else {
+      console.log("Post not found");
+      res.status(404).send("Post not found.");
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("An error occurred while fetching the post.");
+  }
+});
+
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
